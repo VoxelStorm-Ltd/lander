107792757e954dd09edb74706f89c68c2baafb63
SlowRiot <rain.backnet@gmail.com>
2013-12-20 22:27:40 +0000

    more sensitive test rotation

--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 3;
 	static const long MINOR  = 4;
-	static const long BUILD  = 2684;
-	static const long REVISION  = 14757;
+	static const long BUILD  = 2694;
+	static const long REVISION  = 14795;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 4803;
-	#define RC_FILEVERSION 3,4,2684,14757
-	#define RC_FILEVERSION_STRING "3, 4, 2684, 14757\0"
-	static const char FULLVERSION_STRING [] = "3.4.2684.14757";
+	static const long BUILDS_COUNT  = 4829;
+	#define RC_FILEVERSION 3,4,2694,14795
+	#define RC_FILEVERSION_STRING "3, 4, 2694, 14795\0"
+	static const char FULLVERSION_STRING [] = "3.4.2694.14795";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 84;
+	static const long BUILD_HISTORY  = 94;
 	
 
 }
