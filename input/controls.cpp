b0982e1a7900c1624142c7129f10bfd1c7d63a39
SlowRiot <rain.backnet@gmail.com>
2013-12-20 21:28:43 +0000

    delta time tweak

--- input/controls.cpp
+++ input/controls.cpp
@@ -8,7 +8,7 @@ extern astronaut *player;
 
 void pollcontrols(GLFWwindow *thiswindow) {
   //double const amount = 10.0;
-  double const amount = 1.001;
+  double const amount = 1.01;
   double const angle = 1.0;
 
   if(!player->strappeddown) {   // don't check movement while strapped into a seat
