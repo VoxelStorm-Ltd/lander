b9c230fc070271d2fafc7987308b3896dfb4dc70
SlowRiot <rain.backnet@gmail.com>
2013-12-24 04:09:39 +0000

    custom graphics for toggle switch

--- devices/instruments/switch_onoff.cpp
+++ devices/instruments/switch_onoff.cpp
@@ -36,7 +36,7 @@ double switch_onoff::get_mass() {
 
 Vector3d switch_onoff::get_size() {
   /// Return a size for this object, in metres - hardcoded
-  return Vector3d(0.02, 0.02, 0.01);
+  return Vector3d(0.02, 0.02, 0.004);
 }
 
 unsigned int switch_onoff::get_port_in_count() {
@@ -160,7 +160,99 @@ void switch_onoff::get_port_out_sound(unsigned int port __attribute__((__unused_
   }
 }
 
-void device::activate() {
+void switch_onoff::render() {
+  /// Render the switch in its appropriate position
+  glPushMatrix();
+
+  glTranslated(position.x,
+               position.y,
+               position.z);
+
+  Vector3d thissize = get_size();
+  double switch_tip_y;
+  double normal_flip;
+  if(setting == 0) {
+    switch_tip_y = 0.0;
+    //switch_tip_y = -thissize.y;
+    normal_flip = -1.0;
+  } else {
+    switch_tip_y = thissize.y;
+    //switch_tip_y = (thissize.y * 2.0);
+    normal_flip = 1.0;
+  }
+  double const switch_tip_z = thissize.z + thissize.y;
+  double const switch_arm_inset = 0.005;
+
+  // switch body
+  glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, Vector4f(0.2, 0.2, 0.2, 1.0));
+  glMaterialfv(GL_FRONT, GL_SPECULAR,            Vector4f(0.2, 0.2, 0.2, 1.0));
+  glMaterialfv(GL_FRONT, GL_EMISSION,            Vector4f(0.0, 0.0, 0.0, 1.0));
+  glMaterialf(GL_FRONT,  GL_SHININESS,           2.0);                           // 0 to 127
+  glBegin(GL_QUADS);
+  // front
+  glNormal3d(0.0, 0.0, 1.0);
+  glVertex3d(0.0,        0.0,        thissize.z);
+  glVertex3d(thissize.x, 0.0,        thissize.z);
+  glVertex3d(thissize.x, thissize.y, thissize.z);
+  glVertex3d(0.0,        thissize.y, thissize.z);
+  // top
+  glNormal3d(0.0, 1.0, 0.0);
+  glVertex3d(0.0,        thissize.y, 0.0);
+  glVertex3d(0.0,        thissize.y, thissize.z);
+  glVertex3d(thissize.x, thissize.y, thissize.z);
+  glVertex3d(thissize.x, thissize.y, 0.0);
+  // bottom
+  glNormal3d(0.0, -1.0, 0.0);
+  glVertex3d(0.0,        0.0,        0.0);
+  glVertex3d(thissize.x, 0.0,        0.0);
+  glVertex3d(thissize.x, 0.0,        thissize.z);
+  glVertex3d(0.0,        0.0,        thissize.z);
+  // right
+  glNormal3d(1.0, 0.0, 0.0);
+  glVertex3d(thissize.x, 0.0,        0.0);
+  glVertex3d(thissize.x, thissize.y, 0.0);
+  glVertex3d(thissize.x, thissize.y, thissize.z);
+  glVertex3d(thissize.x, 0.0,        thissize.z);
+  // left
+  glNormal3d(-1.0, 0.0, 0.0);
+  glVertex3d(0.0,        0.0,        0.0);
+  glVertex3d(0.0,        0.0,        thissize.z);
+  glVertex3d(0.0,        thissize.y, thissize.z);
+  glVertex3d(0.0,        thissize.y, 0.0);
+
+  // switch arm
+  glNormal3d(0.0, -0.70710678118 * normal_flip, -0.70710678118 * normal_flip);        // 45 degrees ~= 1/sqrt(2)
+  glVertex3d(             switch_arm_inset, thissize.y / 2.0 - 0.002, thissize.z);    // bottom surface
+  glVertex3d(thissize.x - switch_arm_inset, thissize.y / 2.0 - 0.002, thissize.z);
+  glVertex3d(thissize.x - switch_arm_inset, switch_tip_y            , switch_tip_z);
+  glVertex3d(             switch_arm_inset, switch_tip_y            , switch_tip_z);
+  glNormal3d(0.0,  0.70710678118 * normal_flip,  0.70710678118 * normal_flip);        // 45 degrees ~= 1/sqrt(2)
+  glVertex3d(thissize.x - switch_arm_inset, thissize.y / 2.0 + 0.002, thissize.z);    // top surface
+  glVertex3d(             switch_arm_inset, thissize.y / 2.0 + 0.002, thissize.z);
+  glVertex3d(             switch_arm_inset, switch_tip_y            , switch_tip_z);
+  glVertex3d(thissize.x - switch_arm_inset, switch_tip_y            , switch_tip_z);
+  glEnd();
+
+  // switch arm sides
+  glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, Vector4f(0.8, 0.8, 0.8, 1.0));
+  glMaterialfv(GL_FRONT, GL_SPECULAR,            Vector4f(0.8, 0.8, 0.8, 1.0));
+  glMaterialfv(GL_FRONT, GL_EMISSION,            Vector4f(0.0, 0.0, 0.0, 1.0));
+  glMaterialf(GL_FRONT,  GL_SHININESS,           2.0);
+  glBegin(GL_TRIANGLES);
+  glNormal3d(-1.0, 0.0, 0.0);
+  glVertex3d(             switch_arm_inset, thissize.y / 2.0 - 0.002, thissize.z);    // left edge
+  glVertex3d(             switch_arm_inset, switch_tip_y            , switch_tip_z);
+  glVertex3d(             switch_arm_inset, thissize.y / 2.0 + 0.002, thissize.z);
+  glNormal3d( 1.0, 0.0, 0.0);
+  glVertex3d(thissize.x - switch_arm_inset, thissize.y / 2.0 + 0.002, thissize.z);    // right edge
+  glVertex3d(thissize.x - switch_arm_inset, switch_tip_y            , switch_tip_z);
+  glVertex3d(thissize.x - switch_arm_inset, thissize.y / 2.0 - 0.002, thissize.z);
+  glEnd();
+
+  glPopMatrix();
+}
+
+void switch_onoff::activate() {
   /// Activate the current device, whatever that means for this particular one
   setting = (setting + 1) % get_port_in_count();
   std::cout << "New setting: " << setting << std::endl;
--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "23";
+	static const char DATE[] = "24";
 	static const char MONTH[] = "12";
 	static const char YEAR[] = "2013";
 	static const char UBUNTU_VERSION_STYLE[] =  "13.12";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR  = 3;
 	static const long MINOR  = 7;
-	static const long BUILD  = 2972;
-	static const long REVISION  = 16361;
+	static const long BUILD  = 2973;
+	static const long REVISION  = 16371;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 5338;
-	#define RC_FILEVERSION 3,7,2972,16361
-	#define RC_FILEVERSION_STRING "3, 7, 2972, 16361\0"
-	static const char FULLVERSION_STRING [] = "3.7.2972.16361";
+	static const long BUILDS_COUNT  = 5353;
+	#define RC_FILEVERSION 3,7,2973,16371
+	#define RC_FILEVERSION_STRING "3, 7, 2973, 16371\0"
+	static const char FULLVERSION_STRING [] = "3.7.2973.16371";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY  = 72;
+	static const long BUILD_HISTORY  = 73;
 	
 
 }
