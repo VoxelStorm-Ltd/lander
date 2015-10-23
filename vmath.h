5902276aff1e877f5ea2c1c6c1f2b8bef2652c4c
Borealis <slowriot@voxelstorm.com>
2015-10-24 00:09:36 +0100

    Auto-update from libvoxelstorm: switching to gcc's sincos builtin (3a8e3e0)

--- vmath.h
+++ vmath.h
@@ -176,21 +176,21 @@ inline static T constexpr const rad2deg(T const angle_rad) {
 
 template<class T>
 inline static void constexpr sincos_any(T const angle_rad, T &out_sin, T &out_cos) {
-  sincos(angle_rad, &out_sin, &out_cos);
+  __builtin_sincos(angle_rad, &out_sin, &out_cos);
 }
 inline static void constexpr sincos_any(int const angle_rad, int &out_sin, int &out_cos) {
   // if we're only working with integers, assume single precision is enough
   float out_sin_temp = 0.0f;
   float out_cos_temp = 0.0f;
-  sincosf(static_cast<float>(angle_rad), &out_sin_temp, &out_cos_temp);
+  __builtin_sincosf(static_cast<float>(angle_rad), &out_sin_temp, &out_cos_temp);
   out_sin = static_cast<int>(out_sin_temp);
   out_cos = static_cast<int>(out_cos_temp);
 }
 inline static void constexpr sincos_any(float const angle_rad, float &out_sin, float &out_cos) {
-  sincosf(angle_rad, &out_sin, &out_cos);
+  __builtin_sincosf(angle_rad, &out_sin, &out_cos);
 }
 inline static void constexpr sincos_any(long double const angle_rad, long double &out_sin, long double &out_cos) {
-  sincosl(angle_rad, &out_sin, &out_cos);
+  __builtin_sincosl(angle_rad, &out_sin, &out_cos);
 }
 
 template<class T> class Vector2;  // forward declarations
